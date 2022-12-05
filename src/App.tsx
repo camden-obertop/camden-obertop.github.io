1269757a4b664e61f4e21f9a86518c7102472c69
Camden Obertop <camden.obertop@gmail.com>
2022-12-04 20:00:05 -0600

    Modified text

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Welcome to Camden's in-progress website!
         </p>
         <a
           className="App-link"
